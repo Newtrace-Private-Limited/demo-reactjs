8eb546de9f35c041b80117ce679fb3a54c3a4305
newtracechetan <chetan@newtrace.io>
2025-03-05 14:12:13 +0530

    update package.json file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { withAuthenticator } from '@aws-amplify/ui-react';
 function App(signOut) {
   return (
     <div>
-      <h1>Welcome to My Apps</h1>
+      <h1>Welcome to My App2 </h1>
       <div onClick={signOut} style={{ marginTop: '8px', marginLeft: '5px' , cursor: "pointer"}}/> sign out
     </div>
 
